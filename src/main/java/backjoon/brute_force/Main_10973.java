5d71d4a1025e60b23ad432f9ec5f624eb846dd2a
yunhozz <qkrdbsgh1121@naver.com>
2022-09-23 11:27:37 +0900

    이전 순열

---
+++ src/main/java/backjoon/brute_force/Main_10973.java
@@ -0,0 +1,57 @@
+package backjoon.brute_force;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_10973 {
+
+    static int N;
+    static int[] num;
+
+    static boolean prevNum() {
+        int i = num.length - 1;
+        int j = num.length - 1;
+
+        while (i > 0 && num[i - 1] < num[i]) {
+            i--;
+        }
+        if (i == 0) {
+            return false;
+        }
+        while (num[j] > num[i - 1]) {
+            j--;
+        }
+        swap(i - 1, j);
+        j = num.length - 1;
+
+        while (i < j) {
+            swap(i, j);
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static void swap(int idx1, int idx2) {
+        int temp = num[idx1];
+        num[idx1] = num[idx2];
+        num[idx2] = temp;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        num = Arrays.stream(input).mapToInt(Integer::parseInt).toArray();
+
+        if (prevNum()) {
+            for (int  n : num) {
+                System.out.print(n + " ");
+            }
+        } else {
+            System.out.println("-1");
+        }
+    }
+}
