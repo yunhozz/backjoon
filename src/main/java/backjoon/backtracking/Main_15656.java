b787c9db31e251a78c18d89d89e4d5dd136b9015
yunhozz <qkrdbsgh1121@naver.com>
2022-09-23 11:21:44 +0900

    N과 M (7)

---
+++ src/main/java/backjoon/backtracking/Main_15656.java
@@ -0,0 +1,48 @@
+package backjoon.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_15656 {
+
+    private static int[] arr;
+    private static int[] ans;
+    private static StringBuilder sb;
+    private static int N, M;
+
+    private static void func(int idx) {
+        if (idx == M) {
+            for (int i : ans) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            ans[idx] = arr[i];
+            func(idx + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        String[] array = br.readLine().split(" ");
+
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        arr = new int[N];
+        ans = new int[M];
+        sb = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(array[i]);
+        }
+        Arrays.sort(arr);
+        func(0);
+
+        System.out.println(sb);
+    }
+}
