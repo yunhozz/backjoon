153448f54d324c506f202541eed84b40e9e8f43e
yunhozz <qkrdbsgh1121@naver.com>
2022-09-23 11:16:25 +0900

    init

---
+++ src/main/java/backjoon/BackjoonApplication.java
@@ -0,0 +1,13 @@
+package backjoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackjoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackjoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backjoon/BackjoonApplicationTests.java
@@ -0,0 +1,13 @@
+package backjoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackjoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
