16a85701491a281ef849d5d2982b67921c9eb71e
yunhozz <qkrdbsgh1121@naver.com>
2022-09-26 11:33:01 +0900

    부분수열의 합

--- src/main/java/backjoon/backtracking/Main_1182.java
+++ src/main/java/backjoon/backtracking/Main_1182.java
@@ -7,53 +7,39 @@ import java.util.Arrays;
 
 public class Main_1182 {
 
-    static int ans = 0, S;
-    static int[] arr, array;
-    static boolean[] isCheck;
+    static int N, S, ans = 0;
+    static int[] arr;
 
-    static void func(int idx, int size) {
-        if (idx == size) {
-            if (Arrays.stream(array).sum() == S) ans++;
+    static void func(int idx, int sum) {
+        if (idx == N) {
+            if (sum == S) {
+                ans++;
+            }
             return;
         }
-        for (int i = 0; i < isCheck.length; i++) {
-            if (!isCheck[i]) {
-                isCheck[i] = true;
-                array[idx] = arr[i];
-
-                if (isBig(idx)) {
-                    func(idx + 1, size);
-                }
-                isCheck[i] = false;
-            }
-        }
-    }
-
-    static boolean isBig(int idx) {
-        if (idx == 0) {
-            return true;
-        }
-        return array[idx] >= array[idx - 1];
+        // 지금 위치의 원소를 선택하거나, 선택하지 않거나
+        func(idx + 1, sum + arr[idx]);
+        func(idx + 1, sum);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input1 = br.readLine().split(" ");
-        int N = Integer.parseInt(input1[0]);
-        S = Integer.parseInt(input1[1]);
+        N = Integer.parseInt(input1[0]); // 개수
+        S = Integer.parseInt(input1[1]); // 합
 
-        arr = new int[N];
-        isCheck = new boolean[N];
         String[] input2 = br.readLine().split(" ");
         int idx = 0;
+        arr = new int[N];
 
         for (String s : input2) {
             arr[idx++] = Integer.parseInt(s);
         }
-        for (int s = 1; s <= N; s++) {
-            array = new int[s];
-            func(0, s);
+        func(0, 0);
+        if (S == 0) {
+            System.out.println(ans - 1);
+        } else {
+            System.out.println(ans);
         }
-        System.out.println(ans);
     }
 }
