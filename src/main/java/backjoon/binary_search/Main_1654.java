95cfc653989ed9da858bf906fdd664dc768e95ea
yunhozz <qkrdbsgh1121@naver.com>
2022-10-02 15:40:36 +0900

    랜선 자르기

---
+++ src/main/java/backjoon/binary_search/Main_1654.java
@@ -0,0 +1,41 @@
+package backjoon.binary_search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_1654 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int K = Integer.parseInt(input[0]); // 원래 랜선의 개수
+        int N = Integer.parseInt(input[1]); // 필요한 랜선의 개수
+        long[] arr = new long[K];
+        int idx = 0;
+
+        for (int k = 0; k < K; k++) {
+            arr[idx++] = Long.parseLong(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        long min = 0;
+        long max = arr[K - 1] + 1;
+
+        while (min < max) {
+            long mid = (min + max) / 2;
+            long count = 0;
+
+            for (long n : arr) {
+                count += (n / mid);
+            }
+            if (count < N) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+        System.out.println(min - 1);
+    }
+}
