e2fbaa4f35022483327fa8cd0fdd7be2e2ef79e3
yunhozz <qkrdbsgh1121@naver.com>
2022-10-03 19:09:22 +0900

    정수 제곱근

--- src/main/java/backjoon/binary_search/Main_2417.java
+++ src/main/java/backjoon/binary_search/Main_2417.java
@@ -4,28 +4,24 @@ import java.util.Scanner;
 
 public class Main_2417 {
 
-    static long n, ans = Long.MAX_VALUE;
-
-    static void func(long start, long end) {
-        if (start > end) {
-            return;
-        }
-        long mid = (start + end) / 2;
-        long result = (long) Math.pow(mid, 2); // 제곱
-
-        if (result >= n) {
-            ans = Math.min(ans, mid);
-            func(start, mid - 1);
-        } else {
-            func(mid + 1, end);
-        }
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextLong();
+        long n = sc.nextLong();
+        long min = 0;
+        long max = n;
+        long ans = 0;
 
-        func(0, n);
+        while (min <= max) {
+            long mid = (min + max) / 2;
+            long pow = (long) Math.pow(mid, 2);
+
+            if (pow >= n) {
+                ans = mid;
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
         System.out.println(ans);
     }
 }
