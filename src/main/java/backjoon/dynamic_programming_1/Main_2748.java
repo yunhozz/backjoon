026ea7b39ed19a7eee3d293c310a282140b87a7a
yunhozz <qkrdbsgh1121@naver.com>
2022-11-12 18:29:25 +0900

    피보나치 수 2

---
+++ src/main/java/backjoon/dynamic_programming_1/Main_2748.java
@@ -0,0 +1,22 @@
+package backjoon.dynamic_programming_1;
+
+import java.util.Scanner;
+
+public class Main_2748 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] dp = new long[n+2];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        int count = 2;
+
+        while (count <= n) {
+            dp[count] = dp[count-1] + dp[count-2];
+            count++;
+        }
+        System.out.println(dp[n]);
+    }
+}
