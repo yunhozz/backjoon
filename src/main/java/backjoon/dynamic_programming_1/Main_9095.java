79acd18879002a7e5b5427ed7261a9676783de73
yunhozz <qkrdbsgh1121@naver.com>
2022-11-21 17:23:35 +0900

    1,2,3 더하기

---
+++ src/main/java/backjoon/dynamic_programming_1/Main_9095.java
@@ -0,0 +1,25 @@
+package backjoon.dynamic_programming_1;
+
+import java.util.Scanner;
+
+public class Main_9095 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int[] dp = new int[11];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 10; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        for (int i = 0; i < T; i++) {
+            int n = sc.nextInt();
+            System.out.println(dp[n]);
+        }
+    }
+}
